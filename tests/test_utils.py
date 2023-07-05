ce9e3729b3f807121372c6c907da9822790d60dc
Dmitry Nekrasov <demonekr@yandex.ru>
2023-07-05 18:48:03 +0300

    add httpx_mock to TestFetch

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,94 +1,93 @@
+import asyncio
 from secrets import token_hex
+from unittest.mock import AsyncMock, Mock, patch
 
 import pytest
-from httpx import AsyncClient
+from httpx import AsyncClient, Request, Response
+from httpx._exceptions import TimeoutException
+from pytest_httpx import HTTPXMock
 
 from src.utils import count_query, fetch
 
 
-@pytest.mark.asyncio
-async def test_available_url_fetch():
-    """
-    Тестирование запроса к доступной ссылке.
-    """
-    url = 'https://ya.ru'
-    timeout = 1
-    async with AsyncClient() as client:
-        response = await fetch(url=url, client=client, timeout=timeout)
-    assert response.status_code == 200
+class TestFetch():
+    @pytest.mark.asyncio
+    async def test_available_url_fetch(self, httpx_mock: HTTPXMock):
+        """
+        Тестирование функции fetch на выполнение запроса.
+        """
+        httpx_mock.add_response(status_code=200, text='hello')
 
-@pytest.mark.asyncio
-async def test_unavailable_url_fetch():
-    """
-    Тестирование запроса к недоступной ссылке.
-    """
-    url = f'https://{token_hex(8)}.ru'
-    timeout = 1
-    async with AsyncClient() as client:
-        response = await fetch(url=url, client=client, timeout=timeout)
-    assert response is None
+        async with AsyncClient() as client:
+            response = await fetch(url='https://test_url', client=client, timeout=1)
+            assert response.status_code == 200
+            assert response.text == 'hello'
 
-@pytest.mark.asyncio
-async def test_timeout_fetch():
-    """
-    Тестирование запроса с ограничением по таймауту.
-    """
-    url = 'https://ya.ru'
-    timeout = 0.001
-    async with AsyncClient() as client:
-        response = await fetch(url=url, client=client, timeout=timeout)
-    assert response is None
 
-@pytest.mark.asyncio
-async def test_count_query():
-    '''
-    Тестирование функции подсчета количества вхождений слова в тело ответа.
+    @pytest.mark.asyncio
+    async def test_timeout_fetch(self, httpx_mock: HTTPXMock):
+        """
+        Тестирование функции fetch на возврат None при исключении.
+        """
+        httpx_mock.add_exception(TimeoutException('timeout_error'))
 
-    Наверное, этот тест стоило разбить на несколько мелких.
-    '''
-    random_url = f'https://{token_hex(8)}.ru' 
-    urls = [
-        {
-            'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
-            'query': 'pytest'
-        },
-        {
-            'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
-            'query': 'assert'
-        },
-        {
-            'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
-            'query': 'молоко'
-        },
-        {
-            'url': random_url,
-            'query': 'бананы'
-        }
-    ]
-    timeout = 1
-    result = await count_query(urls=urls, timeout=timeout)
+        async with AsyncClient() as client:
+            response = await fetch(url='https://test_url', client=client, timeout=1)
+            assert response is None
 
-    # для ожидаемого результат я подсчитал количество слов в теле ответа
-    # с помощью cURL:
-    # curl -s https://docs.pytest.org/en/7.3.x/contents.html | grep -o 'pytest' | wc -l
-    assert result == [
-        {
-            'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
-            'count': 75,
-            'status': 'ok'
-        }, 
-        {
-            'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
-            'count': 20,
-            'status': 'ok'
-        },
-        {
-            'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
-            'count': 0,
-            'status': 'ok'
-        },
-        {
-            'url': random_url,
-            'status': 'error'
-        }
-    ]
+
+class TestCountQuery():
+
+    @pytest.mark.asyncio
+    async def test_count_query(self):
+        """
+        Тестирование функции подсчета количества вхождений слова в тело ответа.
+
+        Наверное, этот тест стоило разбить на несколько мелких.
+        """
+        random_url = f'https://{token_hex(8)}.ru' 
+        urls = [
+            {
+                'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
+                'query': 'pytest'
+            },
+            {
+                'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
+                'query': 'assert'
+            },
+            {
+                'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
+                'query': 'молоко'
+            },
+            {
+                'url': random_url,
+                'query': 'бананы'
+            }
+        ]
+        timeout = 1
+        result = await count_query(urls=urls, timeout=timeout)
+
+        # для ожидаемого результат я подсчитал количество слов в теле ответа
+        # с помощью cURL:
+        # curl -s https://docs.pytest.org/en/7.3.x/contents.html | grep -o 'pytest' | wc -l
+        assert result == [
+            {
+                'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
+                'count': 75,
+                'status': 'ok'
+            }, 
+            {
+                'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
+                'count': 20,
+                'status': 'ok'
+            },
+            {
+                'url': 'https://docs.pytest.org/en/7.3.x/contents.html',
+                'count': 0,
+                'status': 'ok'
+            },
+            {
+                'url': random_url,
+                'status': 'error'
+            }
+        ]
