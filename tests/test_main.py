5864d75f3927c5210720d38265c2c96288af3703
Dmitry Nekrasov <demonekr@yandex.ru>
2023-07-05 20:47:24 +0300

    add mock to test_count

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,5 +4,26 @@ from src.main import app
 
 client = TestClient(app)
 
-async def test_counts():
-    pass
+def test_counts(mocker):
+    json = {
+        "urls": [
+            {"url": "http://test_url_1.test", "query": "python"},    
+            {"url": "https://test_url_2.test", "query": "django"}
+        ],
+        "max_timeout": 3000
+    }
+    expected = {
+        'urls': [
+            {'url': 'http://test_url_1.test', 'count': 1, 'status': 'ok'},
+            {'url': 'http://test_url_2.test', 'status': 'error'}
+        ]
+    }
+    mock_return_value = [
+        {'url': 'http://test_url_1.test', 'count': 1, 'status': 'ok'},
+        {'url': 'http://test_url_2.test', 'status': 'error'}
+    ]
+
+    mocker.patch('src.main.count_query', return_value=mock_return_value)
+    response = client.post('/counts/', json=json)
+    assert response.status_code == 200
+    assert response.json() == expected
